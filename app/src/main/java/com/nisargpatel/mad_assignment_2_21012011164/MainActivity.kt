8681419bc41d46c2dcd7e6109a6b1c11c374a4cb
NisargPatel99 <139237451+NisargPatel14@users.noreply.github.com>
2023-10-22 18:44:20 +0530

    Third Commit Logo Screen Developed

--- app/src/main/java/com/nisargpatel/mad_assignment_2_21012011164/MainActivity.kt
+++ app/src/main/java/com/nisargpatel/mad_assignment_2_21012011164/MainActivity.kt
@@ -3,9 +3,11 @@ package com.nisargpatel.mad_assignment_2_21012011164
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
