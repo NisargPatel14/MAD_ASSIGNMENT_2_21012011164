ddd9fd1963260b79d4a4975b0af3defba2c97900
NisargPatel99 <139237451+NisargPatel14@users.noreply.github.com>
2023-10-23 16:54:46 +0530

    Sixth Commit Final Screen Created

--- app/src/main/java/com/nisargpatel/mad_assignment_2_21012011164/MainIntroScreen.kt
+++ app/src/main/java/com/nisargpatel/mad_assignment_2_21012011164/MainIntroScreen.kt
@@ -1,13 +1,20 @@
 package com.nisargpatel.mad_assignment_2_21012011164
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainIntroScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_intro_screen)
 
+        val introBtn: Button = findViewById(R.id.button)
+        introBtn.setOnClickListener{
+            val intent = Intent(this,finalScreen::class.java)
+            startActivity(intent)
+        }
 
     }
 }
---
+++ app/src/main/java/com/nisargpatel/mad_assignment_2_21012011164/finalScreen.kt
@@ -0,0 +1,11 @@
+package com.nisargpatel.mad_assignment_2_21012011164
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class finalScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_final_screen)
+    }
+}
