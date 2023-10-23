a32f8b8e5958fc91781942f83be3c699ddb9bccf
NisargPatel99 <139237451+NisargPatel14@users.noreply.github.com>
2023-10-23 11:40:49 +0530

    Fifth Commit part 2 Main Introduction Screen Complete

---
+++ app/src/main/java/com/nisargpatel/mad_assignment_2_21012011164/MainIntroScreen.kt
@@ -0,0 +1,13 @@
+package com.nisargpatel.mad_assignment_2_21012011164
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainIntroScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_intro_screen)
+
+
+    }
+}
